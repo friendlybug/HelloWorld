52236229855fee750f5f83021464538ff4a1b478
nicocastanio <nicocastanio@gmail.com>
2019-07-15 17:46:40 -0300

    add documentation to file.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.example.helloworld;
 
+/**
+ * The HelloWorld class implements an application that
+ * prints "Hello World" to standard output.
+ */
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // display the string. 
     }
 }
